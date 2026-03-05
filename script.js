43784373e715a4f74aa8ce74a8bfdd16f92ed55f
Tirthshah <shahtirth132700@gmail.com>
2026-03-05 13:32:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const observer = new IntersectionObserver(entries=>{
-entries.forEach(entry=>{
-if(entry.isIntersecting){
-entry.target.classList.add("active")
-}
-})
-})
 
-document.querySelectorAll(".reveal")
-.forEach(el=>observer.observe(el))
