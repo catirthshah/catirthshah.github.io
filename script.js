5e82a195a3c7d439ef2aa1873b15229e9d3356fc
Tirthshah <shahtirth132700@gmail.com>
2026-03-05 13:26:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-const items=document.querySelectorAll(".menu-item")
-
-items.forEach(item=>{
-item.addEventListener("mouseenter",()=>{
-
-document.querySelectorAll(".menu-item")
-.forEach(i=>i.classList.remove("active"))
-
-document.querySelectorAll(".tab-content")
-.forEach(c=>c.classList.remove("active"))
-
-item.classList.add("active")
-
-document
-.getElementById(item.dataset.tab)
-.classList.add("active")
-
+const observer = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("active")
+}
 })
 })
 
-
-const slides=document.querySelectorAll(".slide")
-
-let index=0
-
-function showSlide(){
-
-slides.forEach(s=>s.classList.remove("active"))
-
-index++
-
-if(index>=slides.length){
-index=0
-}
-
-slides[index].classList.add("active")
-
-}
-
-setInterval(showSlide,4000)
+document.querySelectorAll(".reveal")
+.forEach(el=>observer.observe(el))
